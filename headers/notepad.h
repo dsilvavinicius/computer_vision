f73cc3ebbcc87d58737042a264913cf47fa7f111
Vinicius da Silva <dsilvavinicius@github.com>
2014-06-24 17:08:22 -0300

    Commiting an Qt example to test the library.

---
+++ headers/notepad.h
@@ -0,0 +1,17 @@
+#include <QMainWindow>
+
+namespace Ui {
+class Notepad;
+}
+
+class Notepad : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit Notepad(QWidget *parent = 0);
+    ~Notepad();
+
+private:
+    Ui::Notepad *ui;
+};
